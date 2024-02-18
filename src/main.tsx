d28b836ab83d384593f450d8744ecc0a60370000
Caspar Rinne <caspar.rinne@gmail.com>
2024-02-18 21:35:52 +0200

    try hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
 import { routes } from './routes.tsx';
 
-const router = createBrowserRouter(routes);
+const router = createHashRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
